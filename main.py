a6036f4df78c0a9e8b32355f0993e959faeb115f
cross.tamomo <cross.tamomo@gmail.com>
2024-10-17 01:28:16 -0500

    add character occurence counter

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
-
 def main():
-    with open("books/frakenstein.txt") as f:
-        file_contents  = f.read()
-        print(f"Number of words in text document: {get_number_of_words(file_contents)}")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-def get_number_of_words(file_text):
-    count_text = file_text.split()
-    return len(count_text)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if item["char"].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    return len(text.split())
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = [{"char": ch, "num": count} for ch, count in num_chars_dict.items()]
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        chars[lowered] = chars.get(lowered, 0) + 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 if __name__ == "__main__":
     main()
